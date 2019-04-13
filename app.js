e5a021837339b35f5bb97425a0b3317c4d303c43
RegisDevOps <wmasters@regis.edu>
2019-04-13 13:07:38 -0600

    Change name

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8082;
 const server = http.createServer((req, res) => {
 	  res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello World. My name is Bill\n');
+	  res.end('Hello World. My name is Inigo\n');
 });
 
 server.listen(port, hostname, () => {
