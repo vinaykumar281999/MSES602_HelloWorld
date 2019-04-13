42cefaa71f196bb2b5347db19eeb7e9e3e2ed60b
RegisDevOps <wmasters@regis.edu>
2019-04-13 13:39:26 -0600

    See jenkins handing of failed test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8082;
 const server = http.createServer((req, res) => {
 	  res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello World. My name is Inigo\n');
+	  res.end('Hello Wordl. My name is Inigo\n');
 });
 
 server.listen(port, hostname, () => {
