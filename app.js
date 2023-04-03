11f612b8257da0de56f0c47951475ca4ea6de486
Bill Masters <wmasters@regis.edu>
2023-04-03 18:46:31 -0400

    Test lab

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8082;
 const server = http.createServer((req, res) => {
 	  res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello World. My name is Tyler... I just changed this message for you\n');
+	  res.end('Hello World. My name is Bill. I just deleted Tylers message');
 });
 
 server.listen(port, hostname, () => {
