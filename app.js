0f1f8e8f0c72c418ec817b4697d91ab1a6f4da6d
Trishpowell <powel625@regis.edu>
2023-04-09 23:17:00 -0400

    git commit -m "Change name to Trish Powell"

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8082;
 const server = http.createServer((req, res) => {
 	  res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello World. My name is Trish');
+	  res.end('Hello World. My name is Trish Powell');
 });
 
 server.listen(port, hostname, () => {
